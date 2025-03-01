46afb303f8feebaea74457f47238803aa0abb9c0
ckirua <gregorferrer@gmail.com>
2025-03-01 00:59:34 +0000

    bucket uploaded

---
+++ fos3/__init__.py
@@ -0,0 +1,13 @@
+from .hetzner import (
+    HetznerS3Bucket,
+    HetznerS3BucketConfiguration,
+    HetznerS3ClientPool,
+)
+
+__all__ = (
+    # Hetzner
+    # -----
+    "HetznerS3Bucket",
+    "HetznerS3BucketConfiguration",
+    "HetznerS3ClientPool",
+)
---
+++ fos3/hetzner/__init__.py
@@ -0,0 +1,11 @@
+from .bucket import (
+    HetznerS3Bucket,
+    HetznerS3BucketConfiguration,
+    HetznerS3ClientPool,
+)
+
+__all__ = (
+    "HetznerS3Bucket",
+    "HetznerS3BucketConfiguration",
+    "HetznerS3ClientPool",
+)
