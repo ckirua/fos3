f7860bd7741a84796098be9640747cf1e73506c1
ckirua <gregorferrer@gmail.com>
2025-03-20 23:56:32 +0000

    release example updated up to current version!

--- scripts/release_example.sh
+++ scripts/release_example.sh
@@ -1,11 +1,11 @@
 # Create an annotated tag (recommended for releases)
-git tag -a v1.0.0 -m "Release version 1.0.0"
+git tag -a v0.0.2 -m "Release version 0.0.2"
 
 # Push the tag to GitHub
-git push origin v1.0.0
+git push origin v0.0.2
 
 # Delete the tag locally
-git tag -d v1.0.0
+git tag -d v0.0.2
 
 # Delete the tag from GitHub
-git push origin --delete v1.0.0
+git push origin --delete v0.0.2
